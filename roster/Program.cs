35e760d80e8b30f2fe8dc12cbec4e964258d6b2f
Blake Mumford <blake.mumford@indoctrinate.com.au>
2017-05-01 11:40:00 +1000

    Add project files.

---
+++ roster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace roster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
